c2f9778c64a2718ba3732803379182ece04f19de
shreya9570 <sheryak543@gmail.com>
2025-03-26 23:34:01 +0530

    Time: 0 ms (100%), Space: 128.4 MB (58.1%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int pi = 0;
         int ni = 1;
+        int n = nums.size();
         vector<int> res(nums.size(), 0);
 
-        for (int n : nums) {
-            if (n > 0) {
-                res[pi] = n;
+        for (int i = 0 ; i< n ; i++) {
+            if (nums[i] > 0) {
+                res[pi] = nums[i];
                 pi += 2;
             } else {
-                res[ni] = n;
+                res[ni] = nums[i];
                 ni += 2;
             }
         }
