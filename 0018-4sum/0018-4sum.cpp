71ba174726609b0fae9ce21f2b6e5f3c39a0e577
shreya9570 <sheryak543@gmail.com>
2025-03-27 10:07:44 +0530

    Time: 24 ms (40.49%), Space: 17.4 MB (59.75%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,89 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
+       int n = nums.size();
         vector<vector<int>> result;
-        
-        sort(nums.begin(), nums.end()); // Sort the array
-        
+       sort( nums.begin() , nums.end());
+       for( int i = 0 ; i < n-3 ; i++){
+        if( i > 0 && nums[i] == nums[i-1] ) continue;
+          for( int j = i+1 ; j < n-2 ; j++){
+         if( j > i+ 1 && nums[j] == nums[j-1] ) continue ;
+           int left = j+1 ;
+           int right = n-1;
+           while( left < right){
+         long long sum = (long long) nums[i] + nums[j] + nums[left] + nums[right];
+         if( sum == target){
+            result.push_back( {nums[i] , nums[j] , nums[left] , nums[right] });
+          while( left < right && nums[left] == nums[left+1]) left++;
+           while( left < right && nums[right] == nums[right-1] ) right-- ;
+           left++;
+           right--;
+          }
+      else if( sum < target ){
+        left++;
+       }
+       else 
+       right--;
+        }
+       }
+       }
+       return result ;
+    }
+};
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       /* int n = nums.size();
+        vector<vector<int>> result;
+    sort(nums.begin(), nums.end()); // Sort the array
         for (int i = 0; i < n - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate i
-            
             for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicate j
-                
                 int left = j + 1, right = n - 1;
-                
-                while (left < right) {
+                 while (left < right) {
                     long long sum = (long long) nums[i] + nums[j] + nums[left] + nums[right]; 
-                    
-                    if (sum == target) {
+                     if (sum == target) {
                         result.push_back({nums[i], nums[j], nums[left], nums[right]});
                         
-                        // Skip duplicate left
+ // Skip duplicate left
                         while (left < right && nums[left] == nums[left + 1]) left++;
                         // Skip duplicate right
                         while (left < right && nums[right] == nums[right - 1]) right--;
@@ -38,3 +101,4 @@ public:
         return result;
     }
 };
+*/
