9987d087cf8f9db51674d99f7b40f5eebe1f544c
shreya9570 <sheryak543@gmail.com>
2025-04-10 00:57:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (50%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -5,17 +5,19 @@ public:
         int high = 0 ;
     for(int i = 0 ; i < s.size() ; i++){
                 if(s[i] == '(' ){ 
-                low ++;
-                high++; }
+                low ++; // low ++ so that hme pata rhe ki kitne open bracet hai 
+                high++; // taki jab ( mile to use reduse kare)
+                }
                 else if(s[i] ==')'){
-                if( low > 0 ) low -- ;
-high-- ; }
+                if( low > 0 ) low -- ; // aek ) mila 
+                        high-- ;// taki usse pahle * aaya hoga to high ko increase kiya higa use bhi reduce karenge  
+                         }
                else if(s[i] == '*'){
-                if( low > 0) 
-low-- ;
-high++;
+                if( low > 0) // Treat '*' as ')'  
+                low-- ; 
+                 high++; // Or treat '*' as '('
                }
-                if(high < 0) return false ;
+                if(high < 0) return false ; // // More ')' than we can balance
     } 
     return low == 0 ;
     
