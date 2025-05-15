d27ef721f341d5e92787fb760263ca21e44bc3ec
shreya9570 <sheryak543@gmail.com>
2025-05-15 11:01:31 +0530

    Time: 8 ms (29.02%), Space: 24 MB (35.26%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // Step 1: Sort the intervals by start time
-        sort(intervals.begin(), intervals.end());
-        
-        vector<vector<int>> merged;
-
-        // Step 2: Traverse using traditional for loop
-        int n = intervals.size();
-        for (int i = 0; i < n; i++) {
-            // If merged is empty OR current interval doesn't overlap with last one in merged
-            if (merged.empty() || merged.back()[1] < intervals[i][0]) {
-                merged.push_back(intervals[i]);
-            } else {
-                // There is overlap, so merge
-                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+        sort(intervals.begin() , intervals.end());
+        vector<vector<int>>merged;
+        for( int i = 0 ; i < intervals.size() ;i++){
+        if(merged.empty() || merged.back()[1] < intervals[i][0]){
+           merged.push_back(intervals[i]); 
+        }
+            else{
+                merged.back()[1] = max(merged.back()[1] , intervals[i][1]);
             }
         }
-
+        
         return merged;
     }
 };
