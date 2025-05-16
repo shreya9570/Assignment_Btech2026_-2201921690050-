90224661dab611612ea6edec8072ac2904c304a3
shreya9570 <sheryak543@gmail.com>
2025-05-16 11:15:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+  void dfs(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ls ){
+      vis[node] = 1 ;
+      ls.push_back(node) ;
+      for( auto it : adj[node]){
+          if(!vis[it]) {
+              dfs(it , adj , vis , ls);
+          }
+      }
+  }
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int n = adj.size();
+       vector<int>vis( n , 0); 
+       int start = 0 ;
+       vector<int>ls ;
+       dfs(start , adj , vis , ls );
+       return ls ;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V;
+        cin >> V;
+        cin.ignore();
+        vector<vector<int>> adj(
+            V); // Use vector of vectors instead of array of vectors.
+
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
+        }
+
+        Solution obj;
+        vector<int> ans = obj.dfs(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
