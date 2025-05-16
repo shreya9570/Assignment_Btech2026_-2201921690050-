49b2371891eac4bfc9b32ea5f630fd588b6120fc
shreya9570 <sheryak543@gmail.com>
2025-05-16 23:02:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -9,26 +9,21 @@ using namespace std;
 class Solution {
   public:
     vector<int> findTwoElement(vector<int>& arr) {
-        int n = arr.size();
-        unordered_map<int, int> freq;
-        int repeating = -1, missing = -1;
-        
-        // Count frequencies
-        for (int i = 0; i < n; i++) {
-            freq[arr[i]]++;
+        // code here
+        long long n = arr.size();
+        long long sn = (n *(n+1))/ 2 ;
+        long long s2n = (n * (n+1)*(2*n+1))/6 ;
+        long long s = 0 , s2 = 0 ;
+        for(int i = 0 ; i < n ; i++){
+        s += arr[i];
+        s2 += (long long)arr[i] * (long long)arr[i];
         }
-        
-        // Find repeating and missing elements
-        for (int i = 1; i <= n; i++) {
-            if (freq[i] == 0) {
-                missing = i;
-            }
-            if (freq[i] == 2) {
-                repeating = i;
-            }
-        }
-        
-        return {repeating, missing};
+        long long val1 = s - sn ;
+        long long val2 = s2 - s2n ;
+        val2 = val2/val1 ;
+        long long x = (val1+val2)/2;
+        long long y = x - val1 ;
+        return {(int)x , (int)y};
     }
 };
 
