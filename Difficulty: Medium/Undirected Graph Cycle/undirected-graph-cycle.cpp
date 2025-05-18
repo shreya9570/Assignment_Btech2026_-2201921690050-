cd0d00686a380e06c9c8ac16bf7072e473a34620
shreya9570 <sheryak543@gmail.com>
2025-05-18 17:01:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -5,71 +5,84 @@ using namespace std;
 
 // } Driver Code Ends
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
-private:
-    bool detectcycles(int node, int parent, vector<int>& vis, vector<vector<int>>& adj) {
-        vis[node] = 1; // Mark node as visited
-        
-        for (auto adjacentnode : adj[node]) {
-            if (!vis[adjacentnode]) { // If neighbor is not visited
-                if (detectcycles(adjacentnode, node, vis, adj)) {
+  public:
+    bool bfs(int start, vector<vector<int>>& adj, vector<bool>& visited) {
+        queue<pair<int, int>> q; // {node, parent}
+        visited[start] = 1;
+        q.push({start, -1});
+
+        while (!q.empty()) {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = 1;
+                    q.push({neighbor, node});
+                } else if (neighbor != parent) {
+                    // If already visited and not parent -> cycle
                     return true;
                 }
-            } 
-            else if (adjacentnode != parent) { // If visited and not parent, cycle detected
-                return true;
             }
         }
         return false;
     }
 
-public:
-    // Function to detect cycle in an undirected graph.
-    bool isCycle(vector<vector<int>>& adj) {
-        int v = adj.size();
-        vector<int> vis(v, 0); // Visited array initialized to 0
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Build adjacency list
+        vector<vector<int>> adj(V);
+        for (auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u); // undirected graph
+        }
 
-        for (int i = 0; i < v; i++) {
-            if (!vis[i]) { // Check for all connected components
-                if (detectcycles(i, -1, vis, adj)) {
+        // Track visited nodes
+        vector<bool> visited(V, 0);
+
+        // Check for cycles in each component
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (bfs(i, adj, visited))
                     return true;
-                }
             }
         }
+
         return false;
     }
 };
 
-
-
-
 //{ Driver Code Starts.
+
 int main() {
     int tc;
     cin >> tc;
+    cin.ignore();
     while (tc--) {
         int V, E;
         cin >> V >> E;
-        vector<vector<int>> adj(V);
-        for (int i = 0; i < E; i++) {
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            edges.push_back({u, v});
         }
+
         Solution obj;
-        bool ans = obj.isCycle(adj);
+        bool ans = obj.isCycle(V, edges);
         if (ans)
-            cout << "1\n";
+            cout << "true\n";
         else
-            cout << "0\n";
+            cout << "false\n";
 
         cout << "~"
              << "\n";
     }
     return 0;
 }
+
 // } Driver Code Ends
