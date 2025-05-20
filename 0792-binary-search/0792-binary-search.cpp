f207318802f1924d42e310be52722531da4c41be
shreya9570 <sheryak543@gmail.com>
2025-05-20 11:31:37 +0530

    Time: 0 ms (100%), Space: 31.3 MB (79.99%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int ans = -1 ;
@@ -9,3 +9,24 @@ public:
       return ans ; 
     }
 };
+*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size() ;
+        int s = 0 ;
+        int e = n-1 ;
+        return bsearch( nums , target , s , e );}
+       int bsearch(vector<int>&arr , int target , int start , int end ){
+            if( start > end) return -1 ;
+            int mid = (start +end)/2 ;
+            if( arr[mid] == target)
+            return mid ;
+            else if( arr[mid] > target){
+            return  bsearch( arr , target , start , mid -1 );
+            }
+            else 
+             return bsearch( arr , target , mid +1 , end );
+        }
+     
+        };
