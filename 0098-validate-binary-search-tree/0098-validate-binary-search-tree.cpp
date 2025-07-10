1d1509b3e0b00d1110fe3016b68e52a3f0ca0452
shreya9570 <sheryak543@gmail.com>
2025-07-10 12:20:37 +0530

    Time: 0 ms (100%), Space: 21.9 MB (46.45%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,105 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ /*
+class Solution {
+public:
+    bool solve( TreeNode* root , long long int lowerBound , long long int upperBound){
+       if( root == NULL){
+        return true ;
+       } 
+       bool cond1 = (root ->val > lowerBound );
+        bool cond2 = (root -> val < upperBound);
+        bool leftans = solve( root -> left , lowerBound , root ->val);
+        bool rightans = solve( root ->right , root ->val , upperBound);
+        if(cond1 && cond2 && leftans && rightans  ){
+            return true ;
+        }
+        else 
+        return false ;
+        
+    }
+     bool isValidBST(TreeNode* root) {
+        long long int lowerBound =  LLONG_MIN;
+        long long int upperBound = LLONG_MAX;
+         bool ans = solve( root , lowerBound , upperBound );
+        return ans ; 
+    }
+};
+     */
+class Solution {
+public:
+  bool solve(TreeNode*root ,long long int lowerbond,  long long int upperbond){
+    if( root == NULL){
+        return true ;
+    }
+    bool cond1 = (root -> val > lowerbond);
+    bool cond2 = ( root ->val < upperbond);
+    bool leftans = solve( root->left  , lowerbond , root -> val );
+    bool rightans = solve( root->right , root -> val  , upperbond); 
+    if(cond1 && cond2 && leftans && rightans  ){
+            return true ;
+        }
+        else 
+        return false ;
+
+  }
+    bool isValidBST(TreeNode* root) {
+        long long int lowerbond = LLONG_MIN ;
+        long long int upperbond = LLONG_MAX;
+        bool ans = solve(root , lowerbond , upperbond);
+        return ans ;
+    }
+};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       /* 
+        if( root == NULL){
+            return true ;
+        }
+        bool cond1 = ( root-> val > lowerBound);
+        bool cond2 = ( root -> val < upperBound);
+        bool leftAns = solve( root -> left  , lowerBound , root -> val);
+        bool rightAns = solve( root -> right , root -> val , upperBound );
+        if(cond1 && cond2 && leftAns && rightAns  ){
+            return true ;
+        }
+        else 
+        return false ;
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        long long int lowerBound =  LLONG_MIN;
+        long long int upperBound = LLONG_MAX;
+        bool ans = solve( root , lowerBound , upperBound );
+        return ans ; 
+    }
+};*/
