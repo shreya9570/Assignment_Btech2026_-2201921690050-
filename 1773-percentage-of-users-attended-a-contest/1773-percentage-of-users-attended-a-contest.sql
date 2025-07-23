44396bc31c0cdda44fb606d8f1aabe19b5be6f02
shreya9570 <sheryak543@gmail.com>
2025-07-24 01:27:15 +0530

    Time: 947 ms (60.76%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,9 +1,13 @@
-# Write your MySQL query statement below
-select 
-contest_id, 
-round(count(distinct user_id) * 100 /(select count(user_id) from Users) ,2) as percentage
-from  Register
-group by contest_id
-order by percentage desc,contest_id
+# Write your MySQL query statement belSELECT 
+  select  contest_id, 
+  ROUND(COUNT(DISTINCT user_id) * 100.0 / (SELECT COUNT(user_id) FROM Users), 2) AS percentage
+FROM 
+  Register
+GROUP BY 
+  contest_id
+ORDER BY 
+  percentage DESC, 
+  contest_id ASC;
+
 #If percentages are same → sort by contest_id ascending.
 #By default, SQL uses ASC (ascending order) when you don’t specify anything.
