361bc332628ab80e4c6bc4fcf84bbe50feb21b10
shreya9570 <sheryak543@gmail.com>
2025-07-24 01:19:31 +0530

    Time: 1051 ms (31.53%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -5,3 +5,5 @@ round(count(distinct user_id) * 100 /(select count(user_id) from Users) ,2) as p
 from  Register
 group by contest_id
 order by percentage desc,contest_id
+#If percentages are same → sort by contest_id ascending.
+#By default, SQL uses ASC (ascending order) when you don’t specify anything.
