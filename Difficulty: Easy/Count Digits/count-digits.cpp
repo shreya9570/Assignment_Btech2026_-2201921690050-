606e6c02c57e78182a73c0a5ffa980998fc84190
shreya9570 <sheryak543@gmail.com>
2025-08-20 22:07:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int countDigits(int n) {
+        // code here
+        if( n == 0)
+        return 1 ;
+        int count = 0 ;
+        while( n > 0){
+            n = n / 10 ;
+            count++ ;
+        }
+        return count ;
+    }
+};
