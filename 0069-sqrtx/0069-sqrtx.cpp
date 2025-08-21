d1d654843e6d132b8bccc751091363accc11239a
shreya9570 <sheryak543@gmail.com>
2025-08-21 12:46:10 +0530

    Time: 8 ms (14.72%), Space: 8.5 MB (85.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//Brute force 
+/*class Solution {
 public:
     int mySqrt(int x) {
         if (x == 0 || x == 1) return x;
@@ -11,3 +12,67 @@ public:
         return i - 1;
     }
 };
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int low = 0, high = x, ans = 0;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+            if (mid * mid == x)
+                return mid;
+            else if (mid * mid < x) {
+                ans = mid;       // possible answer
+               low = mid + 1;  // move right
+            }
+            else {
+                high = mid - 1;  // move left
+            }
+        }
+
+        return ans;
+    }
+};
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+    if( x == 0 || x == 1) return x ;
+    int i = 1 ;
+    while(( long long) i * i <= x ){
+        i++ ;
+    }
+    return i-1 ;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
