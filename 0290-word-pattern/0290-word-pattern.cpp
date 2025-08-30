30b76e3cab62e74fe225179eb7ed6bbc27720bdb
shreya9570 <sheryak543@gmail.com>
2025-08-30 16:22:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (70.32%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        vector<string> words;
-        stringstream ss(s);
-        string word;
-        while (ss >> word) {
-            words.push_back(word);
-        }
+        vector<string> words;     // (empty list of strings initially)
+stringstream ss(s);       // (converts string s = "I love coding" into a stringstream object)
+string word;              // (temporary string holder for each word)
+
+while (ss >> word) {      // (extracts one word at a time from ss into word)
+    words.push_back(word); // (stores that word into vector words)
+}
 
         // If the number of words doesn't match the pattern length, fail
         if (words.size() != pattern.size()) return false;
