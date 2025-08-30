abf77f6f74e8d58dfe690ff37192b9ed20c0921c
shreya9570 <sheryak543@gmail.com>
2025-08-30 19:21:20 +0530

    Time: 13 ms (79.91%), Space: 24.9 MB (76.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,52 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {  
-       int n = strs.size();
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // map: sorted word -> all anagrams
+        unordered_map<string, vector<string>> anagram;
+
+        for (string s : strs) {
+            string key = s;                 // copy original word
+            sort(key.begin(), key.end());   // sort characters to form key
+            anagram[key].push_back(s);      // group word by sorted key
+        }
+
+        vector<vector<string>> result;
+        for (auto& value : anagram) {
+            result.push_back(value.second); // collect groups
+        }
+
+        return result;
+    }
+};
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       /*int n = strs.size();
          unordered_map< string , vector<string>>angram ;
        for( string s : strs){
          string key = s ;  
@@ -18,3 +63,4 @@ public:
     }
 
 };
+*/
