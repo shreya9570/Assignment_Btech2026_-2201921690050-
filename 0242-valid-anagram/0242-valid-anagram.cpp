c880edcd9fc60c79bd224a33efd04d0c45610732
shreya9570 <sheryak543@gmail.com>
2025-08-30 17:19:40 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool isAnagram(string s, string t) {
      
@@ -9,3 +9,23 @@ public:
 }
 
 };  
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    int freqtable[256] = {0};
+      for( int i = 0 ; i< s.size() ; i++){
+        freqtable[s[i]]++;
+     }   //o(n)
+     for( int i = 0 ; i< t.size() ; i ++){
+        freqtable[t[i]]--;
+     }//o(m)
+     for( int i = 0 ; i < 256 ; i ++){
+       if( freqtable[i] != 0 ){
+            return false ;
+       }
+    }
+     return true ;
+    }
+};
