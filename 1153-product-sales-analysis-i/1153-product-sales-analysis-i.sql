22772ff39e28bc151857c328e9cdbcb5d371203b
shreya9570 <sheryak543@gmail.com>
2025-09-06 18:40:07 +0530

    Time: 1514 ms (17.5%), Space: 0B (100%) - LeetHub

---
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+#Without the ON clause, the JOIN operation would produce a Cartesian product
+/*select p.product_name , s.year , s.price from Sales s join Product p on s.product_id = p.product_id;
+#Data Integrity: The ON clause ensures that you only combine rows that have matching values in the specified columns (product_id in this case).
+#Logical Matching: It defines the logical condition for joining the tables, ensuring meaningful results.
+#Control Over Join Behavior: You can customize how tables are joined by using different conditions in the ON clause
+*/
+
+
+select p.product_name , s.year, s.price from Sales s join  Product p on s.product_id = p.product_id ;
