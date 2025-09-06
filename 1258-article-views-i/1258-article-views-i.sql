71c284ea0fdcd4c0ee817b47afc142ba6e0d08ab
shreya9570 <sheryak543@gmail.com>
2025-09-06 15:45:25 +0530

    Time: 1288 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -0,0 +1,70 @@
+# Write your MySQL query statement below
+select author_id as id from Views where author_id = viewer_id 
+group by author_id order by author_id asc ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*SELECT author_id AS id
+FROM views
+WHERE author_id = viewer_id
+GROUP BY author_id order by  author_id asc ;
+  */
