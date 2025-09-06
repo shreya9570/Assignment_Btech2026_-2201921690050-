003dde4ff08fa63fc44b28e6883fa07c680833f3
shreya9570 <sheryak543@gmail.com>
2025-09-06 15:33:39 +0530

    Time: 520 ms (59.26%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -2,4 +2,22 @@
 #select product_id  from Products where low_fats = 'Y' and recyclable = 'Y' ;
 
 
+select product_id from products where low_fats = 'Y' and recyclable = 'Y';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 select product_id from Products where low_fats = 'Y' and recyclable = 'y';
+*/
