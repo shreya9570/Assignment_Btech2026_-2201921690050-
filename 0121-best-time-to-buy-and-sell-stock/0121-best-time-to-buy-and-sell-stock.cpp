54c176210723468384c4b919ccc763d7981cf6f7
shreya9570 <sheryak543@gmail.com>
2025-09-07 11:53:39 +0530

    Time: 0 ms (100%), Space: 97.4 MB (25.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,7 +1,63 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-     int profit = 0 ;
+    int profit = 0 ;
+    int buy = prices[0] ;
+    for(int i = 1 ; i < prices.size() ; i++ ){
+           if( prices[i] < buy ) {
+           buy = prices[i];
+    }
+    else {
+           profit = max( profit ,prices[i] - buy  );
+    }
+    }
+    return profit ;
+    }
+};
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     /*int profit = 0 ;
      int n = prices.size();
      int buy = prices[0];
      
@@ -17,7 +73,7 @@ public:
      return profit;  
     }
 };
-
+*/
 
 
 
