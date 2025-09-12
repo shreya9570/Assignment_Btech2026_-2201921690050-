a5428f353a819644f9b0e047839b1e3b1f14378c
shreya9570 <sheryak543@gmail.com>
2025-09-12 16:06:36 +0530

    Time: 25 ms (5.1%), Space: 12.4 MB (36.94%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,7 +1,41 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-    int n = citations.size();
+        int n = citations.size();
+        sort(citations.begin(), citations.end());  // fix typo
+        
+        for (int i = n; i >= 0; i--) {   // check possible h-index from n to 0
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (citations[j] >= i) {
+                    count++;
+                }
+            }
+            if (count >= i) {   // fix logic
+                return i;       // return i, not j
+            }
+        }
+        return 0;
+    }
+};
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*int n = citations.size();
     sort(citations.begin(), citations.end());
     for (int h = n; h >= 0; h--) {
         int count = 0;
@@ -9,10 +43,11 @@ public:
             if (citations[i] >= h) {
                 count++;
             }
-            if (count >= h)
+            if (count >= h) // as we should have atleast 
                 return h;
         }
     }
     return 0;
 }
 };
+*/
