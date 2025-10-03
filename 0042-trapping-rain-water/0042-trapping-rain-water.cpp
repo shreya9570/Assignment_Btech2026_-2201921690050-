037df25e615663f2e543c3003fe865dec1138f80
shreya9570 <sheryak543@gmail.com>
2025-10-03 14:26:36 +0530

    Time: 0 ms (100%), Space: 27.1 MB (39.47%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int n = height.size();
+        if (n == 0) return 0; // Edge case: no heights
+
+        // Allocate arrays for left and right max heights
+        int* left = new int[n];
+        int* right = new int[n];
+
+        // Fill the left array
+        left[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = max(left[i-1] , height[i]);
+            //left[i] = (left[i - 1] > height[i]) ? left[i - 1] : height[i];
+        }
+
+        // Fill the right array
+        right[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+          right[i] = max( right[i+1] , height[i]);
+          // right[i] = (right[i + 1] > height[i]) ? right[i + 1] : height[i];
+         // waterTrapped += (left[i] < right[i] ? left[i] : right[i]) - height[i];
+        } 
+
+        // Calculate the trapped water
+        int waterTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            waterTrapped +=( min(left[i] , right[i]) - height[i]);
+        }
+
+        // Clean up memory
+        delete[] left;
+        delete[] right;
+
+        return waterTrapped;
+    }
+};
