803615bccdcac1b561b9c27d75e54fd36ac387fc
shreya9570 <sheryak543@gmail.com>
2025-10-05 15:21:17 +0530

    Time: 3 ms (16.54%), Space: 23.8 MB (55.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,6 +2,45 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 int n = nums.size();
+int j = 0 ;
+for(int i = 0 ; i < n ;i++){
+    if(nums[i] != 0 ){
+        nums[j] = nums[i];
+        j++;
+    }
+}
+    while( j < n){
+        nums[j] = 0 ;
+        j++;
+    }
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int n = nums.size();
         int j = 0;
         for( int i = 0 ; i < nums.size() ; i++){
             if( nums[i] != 0  ){
@@ -16,3 +55,4 @@ int n = nums.size();
  
     }
 };
+*/
