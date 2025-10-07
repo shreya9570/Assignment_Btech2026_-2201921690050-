e2310ebbd133edf01be4f370d3d4309e1cc5c149
shreya9570 <sheryak543@gmail.com>
2025-10-07 15:53:29 +0530

    Time: 737 ms (23.11%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
+/*
 SELECT 
   s.user_id,
   ROUND(IFNULL(SUM(c.action = 'confirmed') / COUNT(c.action), 0), 2) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
+*/
+
+
+#So we use 0 inside IFNULL to safely handle missing data and show 0.00 as the confirmation rate instead of NULL.
+ #IFNULL(expression, 0)
+select s.user_id , ROUND(IFNULL(sum(c.action = 'confirmed')/count(c.action) , 0), 2 ) as Confirmation_rate from Signups s left join Confirmations c on s.user_id = c.user_id group by s.user_id ;
