89803a5f5b1b3b91218f93827c326b06271fc68f
shreya9570 <sheryak543@gmail.com>
2025-10-07 17:46:39 +0530

    Time: 1334 ms (23.98%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,8 +1,15 @@
                                                                                                                                                                                                                                                                                                             /*                                                                                                    A S# Write your MySQL query statement below
 select e.unique_id , en.name from Employees en left join EmployeeUNI e on e.id = en.id  ;
-*/
+
 
 select e.unique_id , en.name from Employees en left join EmployeeUNI e on e.id = en.id ;
+*/
+
+
+
+
+select en.unique_id , e.name 
+from Employees e left join EmployeeUNI en on e.id = en.id;
 
 
 
