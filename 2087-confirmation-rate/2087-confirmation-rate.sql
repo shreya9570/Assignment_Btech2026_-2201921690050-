6985959fe0e32a573f4057b530ed4d170a209aa7
shreya9570 <sheryak543@gmail.com>
2025-10-30 00:50:27 +0530

    Time: 659 ms (38.25%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -11,4 +11,4 @@ GROUP BY s.user_id;
 
 #So we use 0 inside IFNULL to safely handle missing data and show 0.00 as the confirmation rate instead of NULL.
  #IFNULL(expression, 0)
-select s.user_id , ROUND(IFNULL(sum(c.action = 'confirmed')/count(c.action) , 0), 2 ) as Confirmation_rate from Signups s left join Confirmations c on s.user_id = c.user_id group by s.user_id ;
+select s.user_id , ROUND(IFNULL(sum(c.action = 'confirmed')/count(c.action) , 0.00), 2 ) as Confirmation_rate from Signups s left join Confirmations c on s.user_id = c.user_id group by s.user_id ;
