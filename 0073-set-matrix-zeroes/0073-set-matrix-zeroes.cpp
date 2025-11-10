8b4962e77c877c281cf6d46611fe8578e2d12ac3
shreya9570 <sheryak543@gmail.com>
2025-11-10 17:15:01 +0530

    Time: 3 ms (10.99%), Space: 18.7 MB (23.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,91 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-           int n = matrix.size();
-           int m = matrix[0].size();
-           vector<int>p(n , 0);
-           vector<int>q(m,0);
-           for( int i = 0 ; i < n ; i++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>p( n , 0);
+        vector<int>q( m , 0);
+        for( int i = 0 ; i < n ; i++){
             for( int j = 0 ; j < m ; j++){
                 if( matrix[i][j] == 0){
                     p[i] = 1 ;
-                    q[j] = 1 ;
+                    q[j] = 1;
                 }
             }
-           }
-           for( int i = 0 ; i < n ; i++){
+        }
+        for( int i = 0 ; i < n; i++){
             for( int j = 0 ; j < m ; j++){
                 if( p[i] == 1 || q[j] == 1){
                     matrix[i][j] = 0 ;
                 }
             }
-           }
+        }
     }
 };
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-            
-          /* vector<int>p(n,0);
-           vector<int>q(m,0);
-           for(int i = 0 ; i < n ; i++){
-              for(int j = 0 ; j < m ; j++){
-                 if( matrix[i][j] == 0){
-                    p[i] = 1 ;
-                    q[j] = 1 ;
-                      }
-           }
-           }
-              for( int i = 0 ; i < n ; i++){
-                for(int j = 0 ; j < m ; j++){
-                     if(p[i]==1|| q[j]==1){
-                        matrix[i][j]=0;
-                     }
-                }
-              }
-    }
-};
-//the total time complexity is: 0(n*m);
-// space complexity = 0(n+m)
-
-*/
