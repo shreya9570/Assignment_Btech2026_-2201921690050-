539eeab3425aa469e164f4b6c160ffcf07e456cb
shreya9570 <sheryak543@gmail.com>
2025-11-11 17:48:25 +0530

    Time: 0 ms (100%), Space: 12.4 MB (69.22%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,14 +4,14 @@ public:
       int left = 0 ;
       int high = nums.size() - 1 ;
       while( left < high){
-         int mid = (left + high) / 2 ;
-         if( nums[mid] > nums[mid+1]){
-            high = mid ;
-         }
-         else {
-            left = mid +1 ;
-         }
-      }  
-      return left;
+        int mid = (left + high) / 2 ;
+            if( nums[mid] > nums[mid +1]){
+                high = mid ;
+            }
+            else {
+                left = mid +1 ;
+            } 
+      }
+      return left ;
     }
 };
