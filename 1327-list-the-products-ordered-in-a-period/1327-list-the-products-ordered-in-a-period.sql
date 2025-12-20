ee955eeb0cb761d208159be5d1e619d2553d4d3d
shreya9570 <sheryak543@gmail.com>
2025-12-20 15:39:12 +0530

    Time: 613 ms (94.98%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -3,7 +3,7 @@ SELECT
     p.product_name,
     SUM(o.unit) AS unit
 FROM Products p
-JOIN Orders o
+left JOIN Orders o
 ON p.product_id = o.product_id
 WHERE o.order_date >= '2020-02-01'
   AND o.order_date < '2020-03-01'
